83b182110a4b4395dc7c420d670171de423c7dc3
heyanle <1371735400@qq.com>
2024-06-02 20:30:06 +0800

    [feature] 1.9

--- extension/extension-api/src/main/java/com/heyanle/extension_api/Extension.kt
+++ extension/extension-api/src/main/java/com/heyanle/extension_api/Extension.kt
@@ -7,13 +7,15 @@ import com.heyanle.easybangumi4.source_api.SourceFactory
  * Created by heyanle on 2024/6/2.
  * https://github.com/heyanLE
  */
-interface Extension : SourceFactory {
+abstract class Extension : SourceFactory {
+
+    var bundle: ExtensionBundle? = null
 
     // 可能会多次调用，如果需要加载 so 包记得加 try catch
     fun onInit(
         application: Application,
-        bundle: ExtensionBundle
-    ){}
+    ){
+    }
 
     fun onDestroy(){}
 
