11607b405810775d8b4337037a531fedf3dfc4de
heyanle <1371735400@qq.com>
2024-06-02 19:56:22 +0800

    [feature] 1.9

--- extension/extension-api/src/main/java/com/heyanle/extension_api/Extension.kt
+++ extension/extension-api/src/main/java/com/heyanle/extension_api/Extension.kt
@@ -12,9 +12,10 @@ interface Extension : SourceFactory {
     // 可能会多次调用，如果需要加载 so 包记得加 try catch
     fun onInit(
         application: Application,
-        path: String
+        bundle: ExtensionBundle
     ){}
 
     fun onDestroy(){}
+
 
 }
